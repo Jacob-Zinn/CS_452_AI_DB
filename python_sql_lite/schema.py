903d42ca1f93c630784de7d54a45aad0acea21e6
cayjobla <cayjobla@gmail.com>
2023-10-04 12:36:30 -0600

    Update to new schema

--- python_sql_lite/schema.py
+++ python_sql_lite/schema.py
@@ -1,53 +1,50 @@
-sql_create_category_table = """
-    CREATE TABLE categories (
-    category_id INT PRIMARY KEY,
-    category_name TEXT
-    );
-"""
+# 
 
-sql_create_menu_table = """
-    CREATE TABLE menu (
-        menu_id INT PRIMARY KEY ,
-        menu_name TEXT,
-        category_id INT,
-        unit_price REAL,
-        FOREIGN KEY (category_id) references categories(category_id)
+sql_create_countries_table = """
+    CREATE TABLE IF NOT EXISTS Countries (
+        CountryID INTEGER PRIMARY KEY AUTOINCREMENT,
+        CountryName TEXT UNIQUE
     );
 """
 
 sql_create_customers_table = """
-    CREATE TABLE customers (
-    customer_id INT PRIMARY KEY ,
-    firstname TEXT,
-    Lastname TEXT,
-    city TEXT
-);
+    CREATE TABLE IF NOT EXISTS Customers (
+        CustomerID INTEGER PRIMARY KEY,
+        CountryID INTEGER,
+        FOREIGN KEY (CountryID) REFERENCES Countries(CountryID)
+    );
 """
 
-sql_create_employee_table = """
-    CREATE TABLE employee (
-        emp_id INT PRIMARY KEY ,
-        firstname TEXT,
-        Lastname TEXT,
-        hiredate TEXT,
-        branch TEXT );
+sql_create_invoices_table = """
+    CREATE TABLE IF NOT EXISTS Invoices (
+        InvoiceNo TEXT PRIMARY KEY,
+        InvoiceDate TEXT,
+        CustomerID INTEGER,
+        FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+    );
 """
 
-sql_create_orders_table = """
-    CREATE TABLE Orders(
-        orderid INT,
-        orderdate TEXT,
-        menu_id INT,
-        quantity INT DEFAULT 0,
-        customer_id INT,
-        delivery_platform TEXT,
-        emp_id INT,
-        FOREIGN KEY (menu_id) references menu(menu_id)
-        FOREIGN KEY (customer_id) references customers(customer_id)
-        FOREIGN KEY (emp_id) references employee(emp_id)
+sql_create_transactions_table = """
+    CREATE TABLE IF NOT EXISTS Transactions (
+        TransactionID INTEGER PRIMARY KEY AUTOINCREMENT,
+        InvoiceNo TEXT,
+        StockCode TEXT,
+        Quantity INTEGER,
+        UnitPrice REAL,
+        TotalPrice REAL AS (Quantity * UnitPrice),
+        TransactionDate TEXT,
+        FOREIGN KEY (InvoiceNo) REFERENCES Invoices(InvoiceNo),
+        FOREIGN KEY (StockCode) REFERENCES Products(StockCode)
+    );
+"""
+
+sql_create_products_table = """
+    CREATE TABLE IF NOT EXISTS Products (
+        StockCode TEXT PRIMARY KEY,
+        Description TEXT
     );
 """
 
 def get_schema():
-    schema = f"{sql_create_category_table}{sql_create_menu_table}{sql_create_customers_table}{sql_create_employee_table}{sql_create_orders_table}"
+    schema = f"{sql_create_countries_table}{sql_create_customers_table}{sql_create_invoices_table}{sql_create_transactions_table}{sql_create_products_table}"
     return schema
