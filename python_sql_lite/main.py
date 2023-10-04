4d0632d103ff2f5ba31587c3aeb1647161e9262a
Jacob Zinn <jacobpzinn@gmail.com>
2023-10-04 14:14:38 -0600

    open ai setup

--- python_sql_lite/main.py
+++ python_sql_lite/main.py
@@ -1,106 +1,54 @@
-import sqlite3
-import pandas as pd
+import argparse
+import openai
+import json
 
-# Read the dataset using pandas
-df = pd.read_excel("Online Retail.xlsx", engine='openpyxl')
+from query import select_from_table
+from schema import get_schema
+from db import create_connection
 
-# Create SQLite connection and cursor
-conn = sqlite3.connect('online_retail.db')
-cursor = conn.cursor()
+DATABASE = "./pythonsqlite.db"
 
-# Create tables
+def main(conn, question):
+    with open("auth.json", "r") as f:
+        auth = json.load(f)
+    # Load your API key from an environment variable or secret management service
+    #openai.api_key = os.getenv(auth['api_key'])
+    openai.api_key = auth['api_key']
+    print(f"Question: {question}")
 
-# 1. Products table
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Products (
-        StockCode TEXT PRIMARY KEY,
-        Description TEXT
+    prompt = f"""
+    
+    Given the following SQL Schema:{get_schema()}
+    Write a SQL query to answer this question: {question}
+    
+    """
+
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant that returns only syntactically correct SQL."},
+        {"role": "user", "content": prompt},
+    ]
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0,
+        max_tokens=600
     )
-""")
-
-# Insert data into Products table
-products = df[['StockCode', 'Description']].drop_duplicates().dropna()
-products.to_sql('Products', conn, if_exists='replace', index=False)
-
-# 2. Customers table
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Customers (
-        CustomerID INTEGER PRIMARY KEY,
-        Country TEXT
-    )
-""")
-
-# Insert data into Customers table
-customers = df[['CustomerID', 'Country']].drop_duplicates().dropna()
-customers.to_sql('Customers', conn, if_exists='replace', index=False)
 
 
 
+    q = response["choices"][0]["message"]["content"]
+
+    print(f"AI-generated SQL query: \n{q}")
+    print("Answer: \n")
+    select_from_table(conn, q)
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--query", type=str, default="Which customer purchased the most number of products?")
+    args = parser.parse_args()
+    conn = create_connection(DATABASE)
 
-# 2. Countries table
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Countries (
-        CountryID INTEGER PRIMARY KEY AUTOINCREMENT,
-        CountryName TEXT UNIQUE
-    )
-""")
+    main(conn, question=args.query)
 
-# Insert data into Countries table
-countries = df[['Country']].drop_duplicates().dropna()
-countries.to_sql('Countries', conn, if_exists='replace', index=False)
-
-# 3. Customers table (modified to refer to Countries table)
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Customers (
-        CustomerID INTEGER PRIMARY KEY,
-        CountryID INTEGER,
-        FOREIGN KEY (CountryID) REFERENCES Countries(CountryID)
-    )
-""")
-
-# Modify the customers dataframe to store CountryID instead of CountryName
-df = df.merge(countries.reset_index(), left_on='Country', right_on='CountryName', how='left')
-customers = df[['CustomerID', 'index']].drop_duplicates().dropna().rename(columns={'index': 'CountryID'})
-customers.to_sql('Customers', conn, if_exists='replace', index=False)
-
-# 4. Invoices table
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Invoices (
-        InvoiceNo TEXT PRIMARY KEY,
-        InvoiceDate TEXT,
-        CustomerID INTEGER,
-        FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
-    )
-""")
-
-# Insert data into Invoices table
-invoices = df[['InvoiceNo', 'InvoiceDate', 'CustomerID']].drop_duplicates().dropna()
-invoices.to_sql('Invoices', conn, if_exists='replace', index=False)
-
-# 5. Transactions table
-cursor.execute("""
-    CREATE TABLE IF NOT EXISTS Transactions (
-        TransactionID INTEGER PRIMARY KEY AUTOINCREMENT,
-        InvoiceNo TEXT,
-        StockCode TEXT,
-        Quantity INTEGER,
-        UnitPrice REAL,
-        TotalPrice REAL AS (Quantity * UnitPrice),
-        TransactionDate TEXT,
-        FOREIGN KEY (InvoiceNo) REFERENCES Invoices(InvoiceNo),
-        FOREIGN KEY (StockCode) REFERENCES Products(StockCode)
-    )
-""")
-
-# Insert data into Transactions table
-transactions = df[['InvoiceNo', 'StockCode', 'Quantity', 'UnitPrice', 'InvoiceDate']]
-transactions.to_sql('Transactions', conn, if_exists='replace', index=False)
-
-
-
-
-# Remember to commit the changes and close the connection
-conn.commit()
-conn.close()
